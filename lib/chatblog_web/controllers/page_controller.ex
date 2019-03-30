8411756266c9976314216192935b4ce6f88bfd81
sebsel <sebsel@seblog.nl>
2019-03-30 17:39:06 +0100

    add view of all entries

--- lib/chatblog_web/controllers/page_controller.ex
+++ lib/chatblog_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule ChatblogWeb.PageController do
   use ChatblogWeb, :controller
+  alias Chatblog.Repo
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    entries =
+      Repo.all(Chatblog.Entry)
+      |> Enum.map(&{&1.channel, {:safe, &1.body}})
+
+    render(conn, "index.html", entries: entries)
   end
 end
